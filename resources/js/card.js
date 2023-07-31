51e4a13ee5affe5089b392f9e83b44ab09ab291e
Andranik Hakobyan <guratr@Andraniks-MacBook-Air.local>
2023-07-31 21:23:48 +0400

    nova v4 support

--- resources/js/card.js
+++ resources/js/card.js
@@ -1,3 +1,5 @@
-Nova.booting((Vue) => {
-    Nova.inertia("NovaServerMetrics", require("./components/Card").default);
+import Card from './components/Card'
+
+Nova.booting((app, store) => {
+    app.component('nova-server-metrics', Card)
 })
